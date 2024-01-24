6c79cd014046b27b1b992c3467e90bde634a20e6
Luke Nonas-Hunter <luke@nonas-hunter.com>
2024-01-24 09:58:22 -0500

    Wrap firefox with nixGL

--- systems/linux/desktop.nix
+++ systems/linux/desktop.nix
@@ -3,36 +3,25 @@ let
   helpers = import ../helpers.nix {
     inherit pkgs;
     inherit lib;
-    inherit  config;
+    inherit config;
     inherit specialArgs;
   };
 in
 {
   imports = [
-    (helpers.linkAppConfig "hypr")
-    (helpers.linkAppConfig "eww")
     (helpers.linkAppConfig "easyeffects")
+    (helpers.linkAppConfig "hypr")
   ];
-
-  fonts.fontconfig.enable = true;
 
   home.packages = [
     (helpers.nixGLMesaWrap pkgs.alacritty)
-    pkgs.firefox
+    (helpers.nixGLMesaWrap pkgs.firefox)
     pkgs.wofi
-    pkgs.eww-wayland
     pkgs.pavucontrol
     pkgs.helvum
     pkgs.easyeffects
     pkgs.slack
     pkgs.spotify
-    pkgs.wpgtk
-
-    pkgs.noto-fonts
-    pkgs.noto-fonts-cjk
-    pkgs.noto-fonts-emoji
-    pkgs.fira-code
-    pkgs.font-awesome
-    pkgs.comic-mono
   ];
+
 }
