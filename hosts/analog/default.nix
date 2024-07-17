0dad2f455666722d4f30c0a2b95d6a1a1ba3d40c
Luke Nonas-Hunter <luke@nonas-hunter.com>
2024-07-17 09:44:12 -0400

    Re-add  analog config

---
+++ hosts/analog/default.nix
@@ -0,0 +1,35 @@
+# Analog
+# An old dog learning new tricks
+
+{
+  inputs,
+  globals,
+  overlays,
+  ...
+}:
+
+inputs.darwin.lib.darwinSystem {
+  system = "aarch64-darwin";
+  specialArgs = { };
+  modules = [
+    ../../modules/common
+    ../../modules/darwin
+    (
+      globals
+      // rec {
+        user = "luke.nonas-hunter";
+      }
+    )
+    inputs.home-manager.darwinModules.home-manager
+    {
+      gui.enable = true;
+      theme = {
+        colors = (import ../../themes/ayu).mirage;
+        darkMode = true;
+      };
+      alacritty.enable = true;
+      vscode.enable = false;
+    }
+
+  ];
+}
